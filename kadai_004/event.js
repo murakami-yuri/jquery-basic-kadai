5f4d1d38ec9a72c142ebe4605d7ff0cab59db739
murakami-yuri <165739107+murakami-yuri@users.noreply.github.com>
2024-08-22 10:35:47 +0900

    Update event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,6 +1,2 @@
-$(function() {
-  console.log('loadイベントが発生しました');
-  $(window).on('scroll', () => {
-    console.log('scrollイベントが発生しました');
-  });
-});
+$(window).on('load', () => {console.log('loadイベントが発生しました');});
+$(window).on('scroll', () => {console.log('scrollイベントが発生しました');});
